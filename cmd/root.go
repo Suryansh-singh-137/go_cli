4ddbcb7096b57a8af5afda6a8e16fd7828fa090a
Surya <suryanshsingh9736@gmail.com>
2026-06-15 00:49:21 +0530

    Add inspect and version commands to the CLI

--- cmd/root.go
+++ cmd/root.go
@@ -1,16 +1,14 @@
 /*
 Copyright © 2026 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 )
-
-
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -25,6 +23,9 @@ to quickly create a Cobra application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+    fmt.Println("Hello from Cobra!")
+},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
