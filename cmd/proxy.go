6174d98246d28eccb9c819ea79195ca1f69b018e
Surya <suryanshsingh9736@gmail.com>
2026-06-17 00:01:00 +0530

    Implement start command for proxy with basic request handling

--- cmd/proxy.go
+++ cmd/proxy.go
@@ -1,11 +1,11 @@
 /*
 Copyright © 2026 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/spf13/cobra"
 )
@@ -20,14 +20,27 @@ and usage of using your command. For example:
 Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("proxy called")
-	},
 }
+var startCmd = &cobra.Command{
+    Use: "start",
+		Run : func(cmd *cobra.Command, args []string) {
+			  fmt.Println("Proxy listening on :8080")
+http.HandleFunc("/",func(w http.ResponseWriter ,  r *http.Request){
+ fmt.Println("Received request:")
+    fmt.Println(r.Method, r.URL.Path)
 
+    w.Write([]byte("Hello from goproxy"))
+})
+    err := http.ListenAndServe(":8080", nil)
+
+    if err != nil {
+        fmt.Println(err)
+    }
+		},
+}
 func init() {
 	rootCmd.AddCommand(proxyCmd)
-
+ proxyCmd.AddCommand(startCmd)
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
