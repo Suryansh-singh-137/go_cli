5fdb604510bd3cb4d5b516cf0bf8dddf7676ef95
Surya <suryanshsingh9736@gmail.com>
2026-06-29 12:41:29 +0530

    Refactor Update function to improve screen handling and key press logic

--- internal/tui/update.go
+++ internal/tui/update.go
@@ -7,9 +7,13 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
     // Is it a key press?
     case tea.KeyMsg:
-	switch msg.String() {
+switch m.screen {// check which screen i hree  , then  handle the key press accoring to the rendered  screen  
 
-	case "up":
+case MainMenu:
+	switch msg.String(){
+
+	
+  case "up":
 		m.selected = (m.selected - 1 + len(menuItems)) % len(menuItems)
 
 	case "down":
@@ -19,9 +23,30 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		return m, tea.Quit
 
 	case "enter":
-		m.screen = Screen(m.selected)
-		 
+	switch m.selected {
+case 0:
+   m.screen =  HTTPClientScreen
+case 1:
+    m.screen =  ProxyScreen
+case 2:
+    return  m , tea.Quit
+}
 	}
+
+case HTTPClientScreen:
+   switch msg.String() {
+
+case "esc":
+    m.screen = MainMenu
+}
+
+case ProxyScreen:
+   switch msg.String() {
+
+case "esc":
+    m.screen = MainMenu
+}
+}
 	}
 	   return m, nil
 }
